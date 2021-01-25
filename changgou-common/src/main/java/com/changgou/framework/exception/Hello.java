3afb1019a2619df84cc040029bd96f46393a7f3c
guoshaobing <1569476832@qq.com>
2021-01-25 20:44:32 +0800

    2021/01/25 2

---
+++ changgou-common/src/main/java/com/changgou/framework/exception/Hello.java
@@ -0,0 +1,12 @@
+package com.changgou.framework.exception;
+
+/**
+ * @ClassName Hello
+ * @Description jljl
+ * @Author 郭少兵
+ * @Date 20:43 2021/1/25
+ * @Version 2.1
+ **/
+public class Hello {
+    int a=01;
+}
