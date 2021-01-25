682b163d90556bd119122c86295d67a1f18b91f8
guoshaobing <1569476832@qq.com>
2021-01-25 20:45:30 +0800

    2021/01/25 2

---
+++ changgou-common/src/main/java/com/changgou/framework/exception/World.java
@@ -0,0 +1,12 @@
+package com.changgou.framework.exception;
+
+/**
+ * @ClassName World
+ * @Description
+ * @Author 郭少兵
+ * @Date 20:44 2021/1/25
+ * @Version 2.1
+ **/
+public class World {
+    int c=0;
+}
