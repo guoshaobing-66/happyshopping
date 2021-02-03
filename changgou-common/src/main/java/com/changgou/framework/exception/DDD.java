f8c90a00b73513fae71d7a7656d74bfdb680dc30
guoshaobing <1569476832@qq.com>
2021-02-03 21:09:11 +0800

    test1 新创建一个DDD类

---
+++ changgou-common/src/main/java/com/changgou/framework/exception/DDD.java
@@ -0,0 +1,12 @@
+package com.changgou.framework.exception;
+
+/**
+ * @ClassName DDD
+ * @Description
+ * @Author 郭少兵
+ * @Date 21:07 2021/2/3
+ * @Version 2.1i
+ **/
+public class DDD {
+    int a=0;
+}
