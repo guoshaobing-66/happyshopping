2ed49643a9bfc02eab67ddee322d775aac1c898e
guoshaobing <1569476832@qq.com>
2021-02-27 18:09:18 +0800

    在FF类中增加了一个cc变量

---
+++ changgou-common/src/main/java/com/changgou/framework/exception/FF.java
@@ -0,0 +1,12 @@
+package com.changgou.framework.exception;
+
+/**
+ * @ClassName FF
+ * @Description
+ * @Author 郭少兵
+ * @Date 18:07 2021/2/27
+ * @Version 2.1
+ **/
+public class FF {
+    int cc;
+}
